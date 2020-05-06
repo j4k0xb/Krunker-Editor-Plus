83f0f146e6a41a662b900e088aafc19a0b0e993a
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2020-05-06 23:05:50 +0200

    Added gui, show real hitbox of rotated objects

--- userscript.user.js
+++ userscript.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Krunker Editor+
-// @version      0.3
+// @version      0.4
 // @description  Custom shortcuts. rounding pos/rot/size to 0.001, Javascript interface
 // @updateURL    https://github.com/j4k0xb/Krunker-Editor-Plus/raw/master/userscript.user.js
 // @downloadURL  https://github.com/j4k0xb/Krunker-Editor-Plus/raw/master/userscript.user.js
 // @author       Jakob#8686
 // @include      /^(https?:\/\/)?(www\.)?(.+)krunker\.io\/editor\.html/
 // @run-at       document-start
-/* globals $ */
+/* globals $, dat */
 // ==/UserScript==
 
 function GM_addStyle(css) {
@@ -23,6 +23,7 @@ function GM_addStyle(css) {
 }
 
 GM_addStyle('.toast{width:200px;height:20px;height:auto;position:absolute;left:50%;margin-left:-100px;bottom:50px;background-color:#383838;color:#f0f0f0;font-family:Calibri;font-size:20px;padding:10px;text-align:center;border-radius:5px;-webkit-box-shadow:0 0 24px -1px #383838;-moz-box-shadow:0 0 24px -1px #383838;box-shadow:0 0 24px -1px #383838}');
+GM_addStyle('#gui { position: absolute; top: 2px; left: 2px }');
 
 const GM_JQ = document.createElement('script');
 GM_JQ.src = 'https://code.jquery.com/jquery-3.5.0.min.js';
@@ -38,14 +39,14 @@ class Mod {
             gui: null,
             three: null
         };
-
-        document.getElementById("infoContainer").innerHTML += `<div id="ownerInfo" class="info" style="position:fixed;top:0;color:steelblue">Editor+ v${this.version}</div>`;
-
-        $(document).on('click', '#copyMap', e => {
-            this.hooks.editor.copyToClipboard(this.hooks.editor.getMapExport());
-            this.showToast("Copied", 1000);
-        });
-        $(document).on('click', '#ownerInfo', e => alert(1));
+        this.defaultSettings = null;
+        this.settings = {
+            showRealHB: false,
+        };
+        this.mainMenu = null;
+        this.visualMenu = null;
+        this.gui = null;
+        this.hitboxes = [];
 
         document.body.innerHTML += '<div class="toast" style="display:none"></div>'
 
@@ -69,15 +70,105 @@ class Mod {
         this.showToast(`Editor+ loaded! Enjoy${user ? ", " + user : ""} ;)`);
     }
 
+    addGui() {
+        this.gui = new dat.GUI;
+        this.gui.domElement.id = 'gui';
+
+        this.mainMenu = this.gui.addFolder("Editor+ v" + this.version);
+        this.mainMenu.open();
+
+        this.visualMenu = this.mainMenu.addFolder("Visual");
+        this.visualMenu.add(this.settings, "showRealHB").name("Show Real Hitbox").onChange(t => {this.setSettings('showRealHB', t)});
+    }
+
+    setupSettings() {
+        this.defaultSettings = JSON.parse(JSON.stringify(this.settings));
+        let ls = this.getSavedVal('editor+');
+        if (ls == null) return;
+        try {
+            JSON.parse(ls);
+        } catch (e) {
+            return;
+        }
+        let jsp = JSON.parse(ls);
+        for (let set in jsp) {
+            this.settings[set] = jsp[set];
+        }
+    }
+
+    setSettings(k, v) {
+        this.settings[k] = v;
+        this.saveVal('editor+', JSON.stringify(this.settings));
+    }
+
+    getSavedVal(t) {
+        const r = "undefined" != typeof Storage;
+        return r ? localStorage.getItem(t) : null;
+    }
+
+    saveVal(t, e) {
+        const r = "undefined" != typeof Storage;
+        r && localStorage.setItem(t, e);
+    }
+
+    onObjectRemoved(e) {
+        if (Object.keys(this.hitboxes).includes(e.uuid)) {
+            const hb = this.hitboxes[e.uuid];
+            this.hooks.editor.scene.remove(hb.owner);
+            delete this.hitboxes[e.uuid];
+        }
+    }
+
+    hasRotation(e) {
+     e = e || this.hooks.editor.objectSelected()
+        const rot = e.rotation;
+        return Math.round(rot.x*1e6) != 0 || Math.round(rot.y*1e6) != 0 || Math.round(rot.z*1e6) != 0;
+    }
+
+    loop() {
+        if (!this.hooks.editor.enabled) return;
+
+        this.hooks.editor.objInsts.forEach(e => {
+           const rot = e.rotation;
+           const showHB = this.hasRotation(e) && e.collidable && this.settings.showRealHB && !["PLACEHOLDER", "SIGN"].includes(e.objType);
+
+           if (Object.keys(this.hitboxes).includes(e.boundingMesh.uuid)) {
+               const hb = this.hitboxes[e.boundingMesh.uuid];
+
+                if (!showHB) {
+                    this.hooks.editor.scene.remove(hb.owner);
+                    delete this.hitboxes[e.boundingMesh.uuid];
+                } else {
+                    hb.owner.scale.x = e.boundingMesh.scale.x;
+                    hb.owner.scale.y = e.boundingMesh.scale.y;
+                    hb.owner.scale.z = e.boundingMesh.scale.z;
+                    hb.owner.position.x = e.boundingMesh.position.x;
+                    hb.owner.position.y = e.boundingMesh.position.y + hb.owner.scale.y/2;
+                    hb.owner.position.z = e.boundingMesh.position.z;
+                }
+           } else if (showHB) {
+               const geometry = new this.hooks.three.BoxGeometry(e.scale.x, e.scale.y, e.scale.z);
+               const material = new this.hooks.three.MeshPhongMaterial({ color: 0x02d10c, opacity: 0.2, transparent: true});
+               const cube = new this.hooks.three.Mesh(geometry, material);
+               this.hooks.editor.scene.add(cube);
+
+               this.hitboxes[e.boundingMesh.uuid] = {"owner": cube};
+           }
+        });
+    }
+
     onEditorInit() {
         const editor = this.hooks.editor;
 
-        const helpHTML = window.windows.filter(w=>w.header=="Help")[0].gen() + "<div style='float: left;width: 50%;'><h4 style='font-size:23px;color:#aacfcf;'>Mod Keybinds</h4><p><b>c</b> = create near cube</p><p><b>t</b> = create near teleporter</p><p><b>g</b> = create near gate</p><p><b>r</b> = create near trigger</p><p><b>shift b</b> = toggle shading</p></div>";
+        const helpHTML = window.windows.filter(w=>w.header=="Help")[0].gen() + "<div style='float: left;width: 50%;'><h4 style='font-size:23px;color:#aacfcf;'>Editor+</h4><p><b>c</b> = create near cube</p><p><b>t</b> = create near teleporter</p><p><b>g</b> = create near gate</p><p><b>r</b> = create near trigger</p><p><b>shift b</b> = toggle shading</p></div>";
         window.windows.filter(w=>w.header=="Help")[0].gen = function() {
             return helpHTML;
         }
 
         this.hooks.editor.toggleProp = this.toggleProp;
+
+        this.setupSettings();
+        this.addGui();
     }
 
     showToast(msg, duration=3000) {
@@ -99,9 +190,6 @@ class Mod {
         let n = this.hooks.editor.camera.getWorldPosition();
         n.add(t.multiplyScalar(1));
         this.hooks.editor.addObject(this.hooks.objectInstance.defaultFromType(id, [Math.round(n.x), Math.round(n.y), Math.round(n.z)]))
-    }
-
-    loop() {
     }
 
     addShortcuts() {
@@ -150,7 +238,7 @@ const observer = new MutationObserver(mutations => {
 
 setTimeout(_=> {
     if ((!unsafeWindow.code || !unsafeWindow.mod || !unsafeWindow.mod.hooks.editor) && confirm("Editor+ couldn't patch the script, please reload the page")) location.reload()
-}, 5000);
+}, 10000);
 
 observer.observe(document.documentElement, {
     childList: true,
@@ -168,13 +256,14 @@ function patchScript(code) {
         .replace('if(this.prefab.dontRound){', 'if(true){') // always dontRound
         .replace(/(Snapping"),1,(\d+),1/g, '$1,0.001,$2,0.001') // gui slider precision
         .replace(/(\(0,1,0\)),Math.abs\(h\)/, '$1,h') // fix group rotation
+        .replace(/(Editor\.prototype\.removeObject\=.*?)(let)/, '$1window.mod.onObjectRemoved(e);$2');
 
     code = `${Mod.toString()}\nwindow.mod = new Mod(${GM.info.script.version});${code}`
 
     let jQCount = 0;
 
     function GM_wait() {
-        if (++jQCount == 50) {
+        if (++jQCount == 100) {
             if (confirm("Editor+ couldn't load jQuery, please reload the page")) location.reload();
             return;
         }
