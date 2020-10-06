20a1b1a7344b409b74afab02f58fadfcb25ffeed
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2020-10-06 19:10:59 +0200

    local auto backup

--- userscript.meta.js
+++ userscript.meta.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name        Krunker Editor+
-// @version     3.8.0
+// @version     3.9.0
 // @author      Jakob#8686
 // @description Custom features for the Krunker Map Editor
 // @include     /^(https?://)?(www.)?(.+)krunker.io/editor.html/
 // @run-at      document-start
+// @grant       GM_setValue
+// @grant       GM_getValue
 // @downloadURL https://github.com/j4k0xb/Krunker-Editor-Plus/raw/master/userscript.user.js
 // @updateURL   https://github.com/j4k0xb/Krunker-Editor-Plus/raw/master/userscript.meta.js
 // ==/UserScript==
