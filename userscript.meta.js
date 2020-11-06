ed19b01d057fa76fdb637d69099610992240c234
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2020-11-06 01:52:04 +0100

    illegal sign character warnings

--- userscript.meta.js
+++ userscript.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Krunker Editor+
-// @version     4.1.6
+// @version     4.1.7
 // @author      Jakob#8686
 // @description Custom features for the Krunker Map Editor
 // @include     /^(https?://)?(www.)?(.+)krunker.io/editor.html/
