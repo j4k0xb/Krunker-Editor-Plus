619dc2a3181b8864d70c16bd01bd77dca177eb9b
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2020-11-22 22:04:32 +0100

    gate/trigger shading, auto groupedit, changelogs

--- userscript.meta.js
+++ userscript.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Krunker Editor+
-// @version     4.8.2
+// @version     4.9.0
 // @author      Jakob#8686
 // @description Custom features for the Krunker Map Editor
 // @include     /^(https?://)?(www.)?(.+)krunker.io/editor.html/
