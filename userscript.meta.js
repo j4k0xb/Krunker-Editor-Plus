6b35d4fbad7294521a326cec47ec5deb6fc19cf5
j4k0xb <55899582+j4k0xb@users.noreply.github.com>
2020-11-29 02:19:32 +0100

    settings bugfixes

--- userscript.meta.js
+++ userscript.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Krunker Editor+
-// @version     4.10.1
+// @version     4.10.2
 // @author      Jakob#8686
 // @description Custom features for the Krunker Map Editor
 // @include     /^(https?://)?(www.)?(.+)krunker.io/editor.html/
